af4fa2bfc724c2c12ae8ce10e40f2fd16c8b3366
boos1993 <boos1993@gmail.com>
2015-02-04 14:49:52 -0600

    Added windo GUI and base switching

--- main.asm
+++ main.asm
@@ -1,9 +1,10 @@
 #include "kernel.inc"
+#include "corelib.inc"
     .db "KEXC"
     .db KEXC_ENTRY_POINT
     .dw start
     .db KEXC_STACK_SIZE
-    .dw 20
+    .dw 40
     .db KEXC_NAME
     .dw name
     .db KEXC_HEADER_END
@@ -17,6 +18,11 @@ start:
 
     ; Allocate and clear a buffer to store the contents of the screen
     pcall(allocScreenBuffer)
+
+    ; Load dependencies
+    kld(de, corelibPath)
+    pcall(loadLibrary)
+
     pcall(clearBuffer)
     kcall(drawScreen)
 
@@ -64,44 +70,87 @@ updateNumber:
         ld l, c
         kld((upperWord), hl)
         kld((lowerWord), ix)
-
     pop de
     pop af
     ret
 
 drawScreen:
     pcall(clearBuffer)
-    
-    ld d, 4
-    ld e, 0
-    kld(hl, (upperWord))
-    kcall(drawBits)
-
     push af
-        ld a, d
-        add a, 9
-        ld d, a
-    pop af
-    ld h, l
-    kcall(drawBits)
-
-    ld d, 4
-    ld e, 10
-    kld(hl, (lowerWord))
-    kcall(drawBits)
-
-    push af
-        ld a, d
-        add a, 9
-        ld d, a
-    pop af
-    ld h, l
-    kcall(drawBits)
-
+    push de
     push hl
     push ix
+
+        kld(hl, windowTitle)
+        ld a, 0x04
+        corelib(drawWindow)
+
+        ld d, 4
+        ld e, 10
+        kld(hl, (upperWord))
+        kcall(drawBits)
+
+        push af
+            ld a, d
+            add a, 9
+            ld d, a
+        pop af
+        ld h, l
+        kcall(drawBits)
+
+        ld d, 4
+        ld e, 19
+        kld(hl, (lowerWord))
+        kcall(drawBits)
+
+        push af
+            ld a, d
+            add a, 9
+            ld d, a
+        pop af
+        ld h, l
+        kcall(drawBits)
+
         ld d, 0
-        ld e, 30
+        ld e, 26
+        ld h, 95
+        ld l, 26
+        pcall(drawLine)
+
+        kld(a, (numberBase))
+        cp 0
+        jr z, .decimalDraw
+
+        kld(a, (numberBase))
+        cp 1
+        jr z, .hexDraw
+
+        kld(a, (numberBase))
+        cp 3
+        jr z, .binaryDraw
+
+.binaryDraw:  
+        ;Draw Binary
+        ld d, 4
+        ld e, 40
+        kld(hl, (upperWord))
+        pcall(drawHexHL)
+        kld(hl, (lowerWord))
+        pcall(drawHexHL)
+        jr .endDraw
+.hexDraw:  
+        ;Draw Hex
+        ld d, 4
+        ld e, 40
+        kld(hl, (upperWord))
+        pcall(drawHexHL)
+        kld(hl, (lowerWord))
+        pcall(drawHexHL)
+        jr .endDraw
+.decimalDraw:     
+        ;Draw Decimal
+        ld d, 4
+        ld e, 40
         kld(hl, (upperWord))
         ld a, h
         ld c, l
@@ -109,20 +158,17 @@ drawScreen:
         ld ixh, h
         ld ixl, l
         pcall(drawDecACIX)
-    
-        ld d, 0
-        ld e, 40
-        kld(hl, (upperWord))
-        pcall(drawHexHL)
-        kld(hl, (lowerWord))
-        pcall(drawHexHL)
+        jr .endDraw
+.endDraw:
+
     pop ix
     pop hl
+    pop de
+    pop af
     ret
 
 ;; drawBits [Text]
 ;;  Draws a byte to the screen
-
 ;; Inputs:
 ;;  H: Byte to be displayed
 ;;  A: Size of element
@@ -163,67 +209,7 @@ drawBits:
 checkKeys:
     push af
 
-        cp k0
-        jr nz, _
-        ld a, 0
-        kcall(updateNumber)
-        _:
-    
-        cp k1
-        jr nz, _
-        ld a, 1
-        kcall(updateNumber)
-        _:
-    
-        cp k2
-        jr nz, _
-        ld a, 2
-        kcall(updateNumber)
-        _:
-    
-        cp k3
-        jr nz, _
-        ld a, 3
-        kcall(updateNumber)
-        _:
-    
-        cp k4
-        jr nz, _
-        ld a, 4
-        kcall(updateNumber)
-        _:
-    
-        cp k5
-        jr nz, _
-        ld a, 5
-        kcall(updateNumber)
-        _:
-    
-        cp k6
-        jr nz, _
-        ld a, 6
-        kcall(updateNumber)
-        _:
-    
-        cp k7
-        jr nz, _
-        ld a, 7
-        kcall(updateNumber)
-        _:
-    
-        cp k8
-        jr nz, _
-        ld a, 8
-        kcall(updateNumber)
-        _:
-    
-        cp k9
-        jr nz, _
-        ld a, 9
-        kcall(updateNumber)
-        _:
-
-        cp kMode
+        cp kClear
         jr nz, _
         push de
         push hl
@@ -231,13 +217,107 @@ checkKeys:
             ld hl, 0x0000
             kld((upperWord), de)
             kld((lowerWord), hl)
+            ld a, 0
+            kcall(updateNumber)
+            kcall(drawScreen)
         pop hl
         pop de
-        _:
+_:
+        cp kYEqu
+        jr nz, _
+        corelib(launchCastle)
+_:
+
+        cp kGraph
+        jr nz, _
+        corelib(launchThreadList)
+_:
+        cp kZoom
+        jr nz, _
+        ld c, 40
+        kld(hl, menu)
+        corelib(showMenu)
+        cp 0xFF
+        jr z, .cancel
+        kld((numberBase), a)
+.cancel:
+        kcall(drawScreen)
+_:
+
+        cp k0
+        jr nz, _
+        ld a, 0
+        kcall(updateNumber)
+_:
+    
+        cp k1
+        jr nz, _
+        ld a, 1
+        kcall(updateNumber)
+_:
+    
+        cp k2
+        jr nz, _
+        ld a, 2
+        kcall(updateNumber)
+_:
+    
+        cp k3
+        jr nz, _
+        ld a, 3
+        kcall(updateNumber)
+_:
+    
+        cp k4
+        jr nz, _
+        ld a, 4
+        kcall(updateNumber)
+_:
+    
+        cp k5
+        jr nz, _
+        ld a, 5
+        kcall(updateNumber)
+_:
+    
+        cp k6
+        jr nz, _
+        ld a, 6
+        kcall(updateNumber)
+_:
+    
+        cp k7
+        jr nz, _
+        ld a, 7
+        kcall(updateNumber)
+_:
+    
+        cp k8
+        jr nz, _
+        ld a, 8
+        kcall(updateNumber)
+_:
+    
+        cp k9
+        jr nz, _
+        ld a, 9
+        kcall(updateNumber)
+_:
 
     pop af
     ret
 
+menu:
+    .db 3
+    .db "Decimal", 0
+    .db "Hex", 0
+    .db "Binary", 0
+windowTitle:
+    .db "Programming Calculator", 0
+corelibPath:
+    .db "/lib/core", 0
+numberBase:
+    .db 0
 upperWord:
     .dw 0
 lowerWord:
